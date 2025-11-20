18c688c15e0902931f3a6361a6772b4402c4c74a
VinhHuuTran <tranhuuvinh1109@gmail.com>
2025-11-20 22:22:45 +0700

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,41 @@
+import { GENERAL_INFO } from '@/lib/data';
+import { GitFork, Star } from 'lucide-react';
+
+interface RepoStats {
+    stargazers_count: number;
+    forks_count: number;
+}
+
+const Footer = async () => {
+    const stargazers_count = 4;
+
+    return (
+        <footer className="text-center pb-5" id="contact">
+            <div className="container">
+                <a
+                    href={`mailto:${GENERAL_INFO.email}`}
+                    className="text-3xl sm:text-4xl font-anton inline-block mt-5 mb-10 hover:underline"
+                >
+                    {GENERAL_INFO.email}
+                </a>
+
+                <div className="">
+                    <a
+                        href="https://github.com/tranhuuvinh1109"
+                        target="_blank"
+                        className="leading-none text-muted-foreground hover:underline hover:text-white"
+                    >
+                        My github
+                        <div className="flex items-center justify-center gap-5 pt-1">
+                            <span className="flex items-center gap-2">
+                                <Star size={18} /> {stargazers_count}
+                            </span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
